50330766917caa6b2ebda94823eb6299321754db
Nick Barry <itsnickbarry@protonmail.ch>
2024-05-16 21:08:10 +0400

    add natspec

--- contracts/StorageRewrite.sol
+++ contracts/StorageRewrite.sol
@@ -2,6 +2,10 @@
 
 pragma solidity ^0.8.0;
 
+/**
+ * @notice Contract for writing arbitrary data to arbitrary storage slots
+ * @dev deploy as EIP-2535 Diamond facet
+ */
 contract StorageRewrite {
     struct Slot {
         uint256 slot;
@@ -14,6 +18,11 @@ contract StorageRewrite {
         AUTHORIZED_SENDER = authorizedSender;
     }
 
+    /**
+     * @notice write arbitrary data to arbitrary storage slots
+     * @dev reverts if sender is not the authorizedSender defined at deployment time
+     * @param slots array of Slot structs indicating which data to write to which storage slots
+     */
     function rewrite(Slot[] calldata slots) external {
         require(msg.sender == AUTHORIZED_SENDER);
 
