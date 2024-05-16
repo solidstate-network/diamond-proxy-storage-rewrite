722772ce91d1714de528c88c2440d1df9be2445c
Nick Barry <itsnickbarry@protonmail.ch>
2024-05-16 21:26:51 +0400

    add function namespace to tests

--- test/StorageRewrite.ts
+++ test/StorageRewrite.ts
@@ -21,38 +21,40 @@ describe('StorageRewrite', () => {
     );
   });
 
-  it('writes arbitrary data to arbitratry storage slot', async () => {
-    const slot0 = BigInt(ethers.hexlify(ethers.randomBytes(32)));
-    const data0 = ethers.hexlify(ethers.randomBytes(32));
-    const slot1 = BigInt(ethers.hexlify(ethers.randomBytes(32)));
-    const data1 = ethers.hexlify(ethers.randomBytes(32));
+  describe('#rewrite((uint256,bytes32)[])', () => {
+    it('writes arbitrary data to arbitratry storage slot', async () => {
+      const slot0 = BigInt(ethers.hexlify(ethers.randomBytes(32)));
+      const data0 = ethers.hexlify(ethers.randomBytes(32));
+      const slot1 = BigInt(ethers.hexlify(ethers.randomBytes(32)));
+      const data1 = ethers.hexlify(ethers.randomBytes(32));
 
-    await instance.connect(authorizedSigner).rewrite([
-      { slot: slot0, data: data0 },
-      { slot: slot1, data: data1 },
-    ]);
+      await instance.connect(authorizedSigner).rewrite([
+        { slot: slot0, data: data0 },
+        { slot: slot1, data: data1 },
+      ]);
 
-    expect(
-      await hre.network.provider.send('eth_getStorageAt', [
-        await instance.getAddress(),
-        ethers.toBeHex(slot0),
-        'latest',
-      ]),
-    ).to.equal(data0);
+      expect(
+        await hre.network.provider.send('eth_getStorageAt', [
+          await instance.getAddress(),
+          ethers.toBeHex(slot0),
+          'latest',
+        ]),
+      ).to.equal(data0);
 
-    expect(
-      await hre.network.provider.send('eth_getStorageAt', [
-        await instance.getAddress(),
-        ethers.toBeHex(slot1),
-        'latest',
-      ]),
-    ).to.equal(data1);
-  });
+      expect(
+        await hre.network.provider.send('eth_getStorageAt', [
+          await instance.getAddress(),
+          ethers.toBeHex(slot1),
+          'latest',
+        ]),
+      ).to.equal(data1);
+    });
 
-  describe('reverts if', () => {
-    it('sender is not authorized sender', async () => {
-      await expect(instance.connect(unauthorizedSigner).rewrite([])).to.be
-        .reverted;
+    describe('reverts if', () => {
+      it('sender is not authorized sender', async () => {
+        await expect(instance.connect(unauthorizedSigner).rewrite([])).to.be
+          .reverted;
+      });
     });
   });
 });
